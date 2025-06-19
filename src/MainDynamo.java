2d6b44faf1f07d839888895db57f33cde51dc350
krylovDR <daniil12244892@mail.ru>
2025-06-20 00:28:37 +0300

    ver 0.0.5.0a добавлена запись в .csv файл

--- src/MainDynamo.java
+++ src/MainDynamo.java
@@ -1,12 +1,32 @@
+
+import java.util.LinkedList;
+import java.util.List;
+
+import io.CSVHandler;
 import simulation.DynamoPerformer;
 
 public class MainDynamo {
     
     public static void main(String[] args) {
-        var sim = new DynamoPerformer(10, 5, 5, 0.4);
-        sim.simulate(1_000_000, 1);
-        print(sim);
-        print("Average AoI: " + sim.getAvgAOI());
+        int n = 50;         // количество узлов в системе
+        int w = 25;         // количество узлов в кворуме записи
+        int r = 25;         // количество узлов в кворуме чтения
+        double q = 0.3;     // вероятность успешной записи
+
+        List<Object> valuesX = new LinkedList<>();  // для построения графиков, ось X
+        List<Object> valuesY = new LinkedList<>();  // для построения графиков, ось Y
+
+        // увеличение w от 1 до n
+        for (w = 1; w <= n; w++) {
+            var sim = new DynamoPerformer(n, w, r, q);
+            sim.simulate(500_000, 1);
+            valuesX.add(sim.getAvgAOI());
+            valuesY.add(w);
+            print("w = " + w + ", Average AoI: " + sim.getAvgAOI());
+        }
+        CSVHandler.createCSV("outX", valuesX);
+        CSVHandler.createCSV("outY", valuesY);
+        
     }
 
     public static void print(Object o) {
---
+++ src/io/CSVHandler.java
@@ -0,0 +1,28 @@
+package io;
+
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CSVHandler {
+
+    /**
+     * Запись данных {@code values} в {@code filename}.csv файл.
+     * 
+     * @param filename - название файла (без .csv)
+     * @param values - данные подлежащие записи в файл
+     */
+    public static void createCSV(String filename, List<Object> values) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filename + ".csv"))) {
+            writer.writeNext(new String[] {"values"});  // заголовок
+
+            for (Object value : values) {
+                writer.writeNext(new String[] {value.toString()});
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
